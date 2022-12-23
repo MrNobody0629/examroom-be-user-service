0dcc87b375289843227742251b5be85683db007b
Anuj Verma <109571920+anuj-antino@users.noreply.github.com>
2022-12-23 10:25:19 +0530

    accept only pdf files

--- src/components/files/file.service.js
+++ src/components/files/file.service.js
@@ -15,14 +15,35 @@ if (!('fileServerConfig' in config)) {
 const { fileServerConfig } = config;
 
 const uploadFile = async (file) => {
+  if (!['application/pdf'].includes(file.mimetype)) {
+    throw new AppError(
+      'uploadFile',
+      'Only pdf files is acceptable',
+      'custom',
+      500
+    );
+  }
+
   file.buffer.name = file.originalname;
   const formData = new FormData();
   formData.append('file', file.buffer);
-  const response = await axios.post(
-    `${fileServerConfig.url}/api/v1/file/upload`,
-    formData
-  );
-  return response.data;
+  const response = await axios
+    .post(`${fileServerConfig.url}/api/v1/file/upload`, formData)
+    .then((data) => {
+      return data.data;
+    })
+    .catch((error) => {
+      return error.response.data;
+    });
+  if (response.status == 'fail') {
+    throw new AppError(
+      'updateKycDetails',
+      'File server not responding',
+      'custom',
+      500
+    );
+  }
+  return response;
 };
 
 module.exports = {
